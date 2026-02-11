4199fe8ec110bf1da7412a6de5007e62670869cb
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-02-11 03:26:47 +0300

    шлюзы теперь светятся

--- Content.Client/Doors/AirlockSystem.cs
+++ Content.Client/Doors/AirlockSystem.cs
@@ -97,8 +97,8 @@ public sealed class AirlockSystem : SharedAirlockSystem
                     (state == DoorState.Closing
                 ||  state == DoorState.Opening
                 ||  state == DoorState.Denying
-                || (state == DoorState.Open && comp.OpenUnlitVisible)
-                || (_appearanceSystem.TryGetData<bool>(uid, DoorVisuals.ClosedLights, out var closedLights, args.Component) && closedLights))
+                || state == DoorState.Open && comp.OpenUnlitVisible
+                || state == DoorState.Closed && comp.OpenUnlitVisible) // Erida
                     && !boltedVisible && !emergencyLightsVisible;
         }
 
